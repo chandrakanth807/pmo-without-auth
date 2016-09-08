ea8b873abbec884b599b0f6c1e87f209cf05337f
chandrakanth807 <chandrakanth@razorthink.net>
2016-09-08 15:25:42 +0530

    added startdate enddate and completedate fields

--- src/main/java/com/razorthink/pmo/bean/projecturls/jira/SprintValue.java
+++ src/main/java/com/razorthink/pmo/bean/projecturls/jira/SprintValue.java
@@ -1,10 +1,15 @@
 package com.razorthink.pmo.bean.projecturls.jira;
 
+import java.util.Date;
+
 public class SprintValue {
 
     private Integer id;
     private String state;
     private String name;
+    private Date startDate;
+    private Date endDate;
+    private Date completeDate;
 
     public Integer getId() {
         return id;
@@ -28,5 +33,29 @@ public class SprintValue {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCompleteDate() {
+        return completeDate;
+    }
+
+    public void setCompleteDate(Date completeDate) {
+        this.completeDate = completeDate;
     }
 }
